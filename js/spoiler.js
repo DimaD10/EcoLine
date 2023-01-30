7a212263fd342831cdcdf7d18d8df0e5b73cc55e
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-01-30 18:15:25 +0200

    spoiler fixed

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,10 +1,37 @@
-window.addEventListener('click', (e) => {
-    if(e.target.dataset.action === "show"){
-        const spoiler = e.target.closest(".select");
-        spoiler.classList.toggle("spoiler-parrent-active");
-        const button = e.target.querySelector(".select__icon");
-        button.classList.toggle("spoiler__button-active");
-        const content = spoiler.querySelector('.select__body');
-        content.classList.toggle('spoiler-opened');
+
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
     }
-});
+
+    function selectChoose() {
+        let text = this.innerHTML,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.current-box');
+        currentText.innerHTML = text;
+        select.classList.remove('is-active');
+
+    }
+
+    document.addEventListener("click", (e) => {
+        if (!e.target.classList.contains("select") && !e.target.classList.contains("select__header")) {
+            selectParent = document.querySelectorAll('.select');
+            selectParent.forEach(el => {
+                el.classList.remove("is-active");
+            });
+        }
+    });
+};
+select();
+
